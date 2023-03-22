ff85b35e183c6e1c514aa21c84bbeae26e4bfae8
Martin Wang <65059824+MartinGsus99@users.noreply.github.com>
2023-03-22 18:57:22 +0800

    完成搜索框样式

--- sca/src/components/Filter/index.jsx
+++ sca/src/components/Filter/index.jsx
@@ -4,39 +4,98 @@ import {
     Button,
     Radio,
     Select,
+    DatePicker,
 } from 'antd';
+
+const { RangePicker } = DatePicker;
+const onChange = (value, dateString) => {
+    console.log('Selected Time: ', value);
+    console.log('Formatted Selected Time: ', dateString);
+};
+const onOk = (value) => {
+    console.log('onOk: ', value);
+};
+
+const { Option } = Select;
 class DynamicFilter extends Component {
     constructor(props) {
         super(props);
     }
-    state = {}
-
-    initFilterForm() {
-        const { formList } = this.props.formList;
-        const formItemList = [];
-        if(formList.length>0){
-            formList.map((item) => {
-                if (item.type == 'input') {
-                    const inputItem = (
-                        <Form.Item label={item.label} name={item.key} >
-                           <Input></Input>
-                        </Form.Item>
-                    )
-                    formItemList.push(inputItem);
-                }
-            })
-        }
-
-
-        return formItemList;
+    state = {
 
     }
 
+    getOptionList(data, name) {
+        if (!data) {
+            return [];
+        }
+        const options = [];
+        data.map(item => {
+            options.push(
+                <Option value={item.value} key={item.value}>
+                    {item.label}
+                </Option>
+            );
+        });
+        return options;
+    }
+
+    initFilterForm = () => {
+        const { formList } = this.props;
+        console.log("Form", formList);
+        const formItemList = [];
+        if (formList.length > 0) {
+            formList.map((item) => {
+                if (item.type == 'input') {
+                    const inputItem = (
+                        <Form.Item key={item.key} label={item.label} name={item.key} >
+                            <Input placeholder={item.placeholder}></Input>
+                        </Form.Item>
+                    )
+                    formItemList.push(inputItem);
+                } else if (item.type == 'select') {
+                    const selectItem = (
+                        <Form.Item key={item.key} label={item.label} name={item.key} >
+                            <Select placeholder={item.placeholder} style={{ width: 160 }}>
+                                {
+                                    this.getOptionList(item.options)
+                                }
+                            </Select>
+                        </Form.Item>
+                    )
+                    formItemList.push(selectItem);
+                } else if (item.type == 'daterange') {
+                    const datePicker = (
+
+                        <Form.Item key={item.key} label={item.label} name={item.key}>
+                            <RangePicker
+                                showTime={{
+                                    format: 'HH:mm',
+                                }}
+                                format="YYYY-MM-DD HH:mm"
+                                onChange={onChange}
+                                onOk={onOk}
+                            ></RangePicker>
+                        </Form.Item>
+
+                    )
+                    formItemList.push(datePicker);
+                }
+            })
+        }
+        return formItemList;
+    }
 
     render() {
         return (
-            <Form>
-              
+            <Form layout='inline'>
+                {
+                    this.initFilterForm()
+                }
+                <Form.Item>
+                    <Button type="primary" onClick={this.onSubmit} style={{ margin: '0 20px' }}>查询</Button>
+                    <Button type="primary" onClick={this.reset}>清空</Button>
+                </Form.Item>
             </Form>
         );
     }
